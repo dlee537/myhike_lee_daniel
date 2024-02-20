77bf9aa198ed10bd1243ec4a02af3f5974e2d22e
Daniel Lee <dancheuklee@gmail.com>
2024-02-20 10:23:07 -0800

    Complete demo_07

--- scripts/firebaseAPI_TEAM99.js
+++ scripts/firebaseAPI_TEAM99.js
@@ -2,12 +2,12 @@
 //  Your web app's Firebase configuration
 //----------------------------------------
 var firebaseConfig = {
-    apiKey: ,
-    authDomain: ,
-    projectId: ,
-    storageBucket: ,
-    messagingSenderId: ,
-    appId:
+    apiKey: "AIzaSyDxgHM3salg-I65muWqCrVHFOmlR9bKB-k",
+    authDomain: "comp1800-202410-demo-f87e6.firebaseapp.com",
+    projectId: "comp1800-202410-demo-f87e6",
+    storageBucket: "comp1800-202410-demo-f87e6.appspot.com",
+    messagingSenderId: "155012527728",
+    appId: "1:155012527728:web:00d5406948029a9bbfe6b5"
 };
 
 //--------------------------------------------
